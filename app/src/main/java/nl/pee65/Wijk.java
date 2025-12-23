88076a7f88fb86cfcde9e1b166a75b0786a15aca
mad11max <mad11max@yahoo.com>
2025-12-23 11:59:13 +0100

    UPD 2026

--- app/src/main/java/nl/pee65/Wijk.java
+++ app/src/main/java/nl/pee65/Wijk.java
@@ -19,47 +19,47 @@ import android.util.Log;
  *
  */
 public enum Wijk {
-    PLASWIJK(  Calendar.MONDAY , Calendar.FRIDAY, Calendar.THURSDAY, Calendar.MONDAY,       0,         !false,false,true,!true,false,false,"21-4-2014=19-4-2014, 29-5-2014=31-5-2014, 25-12-2014=20-12-2014, 06-04-2015=04-04-2015, 14-05-2015=16-05-2015")
+    PLASWIJK(  Calendar.FRIDAY , Calendar.WEDNESDAY, Calendar.MONDAY, Calendar.THURSDAY,       0,         false,false,true,true,false,false,"21-4-2014=19-4-2014, 29-5-2014=31-5-2014, 25-12-2014=20-12-2014, 06-04-2015=04-04-2015, 14-05-2015=16-05-2015")
     //                  groen             blauw                oranje           grijs      zak      even groe,  blau ,ora,grij,zak,zakon
 
 
 
 
-    , BLOEMENDAAL(Calendar.MONDAY , Calendar.WEDNESDAY, Calendar.TUESDAY, Calendar.MONDAY, 0,               !false,true,false,!true,false,false,"9-6-2014=7-6-2014, 27-04-2015=25-04-2015,25-05-2015=30-05-2015")
+    , BLOEMENDAAL(Calendar.FRIDAY , Calendar.MONDAY, Calendar.TUESDAY, Calendar.THURSDAY, 0,               false,false,false,!true,false,false,"9-6-2014=7-6-2014, 27-04-2015=25-04-2015,25-05-2015=30-05-2015")
 
 
     , GOUDA_NOORD(Calendar.WEDNESDAY , Calendar.FRIDAY, Calendar.THURSDAY, 0, 0,                        !false,true,false,!true,false,false,"9-6-2014=7-6-2014, 27-04-2015=25-04-2015, 05-05-2015=09-05-2015, 25-05-2015=30-05-2015")
 
 
-    , ACHTERWILLENS(Calendar.WEDNESDAY , Calendar.FRIDAY, Calendar.THURSDAY, Calendar.WEDNESDAY, 0      ,!false,!false,true,!true,false,false,"29-5-2014=31-5-2014, 25-12-2014=20-12-2014, 26-12-2014=27-12-2014, 05-05-2015=09-05-2015, 25-12-2015=19-12-2015")
+    , ACHTERWILLENS(Calendar.WEDNESDAY , Calendar.FRIDAY, Calendar.MONDAY, Calendar.THURSDAY, 0      ,!false,false,true,true,false,false,"29-5-2014=31-5-2014, 25-12-2014=20-12-2014, 26-12-2014=27-12-2014, 05-05-2015=09-05-2015, 25-12-2015=19-12-2015")
 
 
-    , NIEUWE_PARK(Calendar.MONDAY , Calendar.FRIDAY, Calendar.THURSDAY, Calendar.MONDAY, 0              ,!false,false,true,!true,false,false,"29-5-2014=31-5-2014, 25-12-2014=20-12-2014, 26-12-2014=27-12-2014, 14-05-2015=16-05-2015, 25-12-2015=19-12-2015")
+    , NIEUWE_PARK(Calendar.FRIDAY , Calendar.THURSDAY, Calendar.MONDAY, Calendar.THURSDAY, 0              ,false,false,true,!true,false,false,"29-5-2014=31-5-2014, 25-12-2014=20-12-2014, 26-12-2014=27-12-2014, 14-05-2015=16-05-2015, 25-12-2015=19-12-2015")
 
 
     , BINNENSTAD_NOORD(Calendar.MONDAY     , Calendar.MONDAY   , Calendar.WEDNESDAY,-1                               , 0,            true,true,true,!true,!true, false,"21-4-2014=22-4-2014, 9-6-2014=10-6-2014, 24-12-2014=23-12-2014, 05-05-2015=06-05-2015")
 
 //                  groen             blauw                oranje           grijs           zak      even groe,  blau ,ora,grij,zak,zakon
 
-    , KORT_HAARLEM(Calendar.WEDNESDAY , Calendar.FRIDAY, Calendar.THURSDAY, Calendar.WEDNESDAY,0       ,!false,false,true,!true,false,false,"29-5-2014=31-5-2014, 25-12-2014=20-12-2014, 26-12-2014=27-12-2014, 14-05-2015=16-05-2015, 25-12-2015=19-12-2015")
+    , KORT_HAARLEM(Calendar.WEDNESDAY , Calendar.THURSDAY, Calendar.MONDAY, Calendar.WEDNESDAY,0       ,!false,false,true,!true,false,false,"29-5-2014=31-5-2014, 25-12-2014=20-12-2014, 26-12-2014=27-12-2014, 14-05-2015=16-05-2015, 25-12-2015=19-12-2015")
 
 
    // , GOUDA_OOST(Calendar.WEDNESDAY , Calendar.FRIDAY, Calendar.TUESDAY,0,0                            ,!false,!false,!true,true,false,false,"05-05-2015=09-05-2015")
 
 
-    , GOVERWELLE_WEST(Calendar.WEDNESDAY , Calendar.FRIDAY, Calendar.TUESDAY, Calendar.WEDNESDAY, 0    ,!false,!false,!true,!true,false,false,"05-05-2015=09-05-2015")
+    , GOVERWELLE_WEST(Calendar.WEDNESDAY , Calendar.FRIDAY, Calendar.TUESDAY, Calendar.MONDAY, 0    ,!false,!false,!true,!true,false,false,"05-05-2015=09-05-2015")
 
 
-    , GOVERWELLE_OOST(Calendar.WEDNESDAY , Calendar.FRIDAY, Calendar.TUESDAY, Calendar.WEDNESDAY, 0     ,!false,false,!true,!true,false,false,"29-5-2014=31-5-2014, 25-12-2014=20-12-2014, 26-12-2014=27-12-2014,01-01-2015=03-01-2015,14-05-2015=16-05-2015,05-05-2015=09-05-2015, 25-12-2015=19-12-2015")
+    , GOVERWELLE_OOST(Calendar.WEDNESDAY , Calendar.MONDAY, Calendar.TUESDAY, Calendar.MONDAY, 0     ,!false,false,!true, true,false,false,"29-5-2014=31-5-2014, 25-12-2014=20-12-2014, 26-12-2014=27-12-2014,01-01-2015=03-01-2015,14-05-2015=16-05-2015,05-05-2015=09-05-2015, 25-12-2015=19-12-2015")
 
 
   //  , BINNENSTAD_ZUID(0   , 0 , Calendar.WEDNESDAY, 0                                    ,Calendar.MONDAY              ,false,false,true,true,true,true,"21-4-2014=22-4-2014, 9-6-2014=10-6-2014, 24-12-2014=23-12-2014, 06-04-2015=07-04-2015, 27-04-2015=28-04-2015, 25-05-2015=26-05-2015")
 
 
-    , KORTE_AKKEREN(Calendar.WEDNESDAY , Calendar.TUESDAY, Calendar.THURSDAY, Calendar.WEDNESDAY, 0      ,!false,false,!true,!true,false,false,"21-4-2014=19-4-2014, 9-6-2014=7-6-2014, 01-01-2015=03-01-2015,06-04-2015=04-04-2015,27-04-2015=25-04-2015, 05-05-2015=09-05-2015, 25-05-2015=30-05-2015")
+    , KORTE_AKKEREN(Calendar.WEDNESDAY , Calendar.FRIDAY, Calendar.MONDAY, Calendar.FRIDAY, 0      ,!false,false,!true,true,false,false,"21-4-2014=19-4-2014, 9-6-2014=7-6-2014, 01-01-2015=03-01-2015,06-04-2015=04-04-2015,27-04-2015=25-04-2015, 05-05-2015=09-05-2015, 25-05-2015=30-05-2015")
 
 //                          groen             blauw                oranje           grijs
-    , STOLWIJKERSLUIS(Calendar.WEDNESDAY , Calendar.FRIDAY, Calendar.TUESDAY, Calendar.WEDNESDAY, 0      , true, false, false, false,false,false,"29-5-2014=31-5-2014, 25-12-2014=20-12-2014, 01-01-2015=03-01-2015,05-05-2015=09-05-2015, 14-05-2015=16-05-2015")
+    , STOLWIJKERSLUIS(Calendar.WEDNESDAY , Calendar.FRIDAY, Calendar.TUESDAY, Calendar.MONDAY, 0      , true, false, false, false,false,false,"29-5-2014=31-5-2014, 25-12-2014=20-12-2014, 01-01-2015=03-01-2015,05-05-2015=09-05-2015, 14-05-2015=16-05-2015")
     ;
 
 
